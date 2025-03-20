22ee9c1355953ad84aaa8ca968dfe98385a84dc9
Maurycy Szmuc <mauryc_s@wp.pl>
2025-03-20 21:31:17 +0100

    (#9) Test modelu YOLOv8 z gotowym datasetem cirucitów

--- schematy_code/model/main.py
+++ schematy_code/model/main.py
@@ -6,6 +6,8 @@ from tqdm import tqdm
 import os
 import sys
 
+#dataset z https://universe.roboflow.com/sarl/electronic-circuits-ifz6c/dataset/1
+#dodaj wyeksportuj do tego folderu
 model = YOLO("yolov8n.yaml")
 result = model.train(data ="data.yaml",epochs = 1)
 print(result)
