8506b4812b633302d6aff391a09a7dc917bedc77
Maurycy Szmuc <mauryc_s@wp.pl>
2025-03-27 08:04:38 +0100

    #9 Wygenerowano model

--- schematics/block_detector/test_yolo/main.py
+++ schematics/block_detector/test_yolo/main.py
@@ -6,23 +6,26 @@ import matplotlib.pyplot as plt
 #dataset z https://universe.roboflow.com/sarl/electronic-circuits-ifz6c/dataset/1
 #dodaj wyeksportuj do tego folderu
 import torch 
-torch.cuda.empty_cache()  # clear memory
-model = YOLO("yolov8n.pt")
-model.to("cuda")
-dataset_path = "../dataset"
-results = model.train(
-    data = f"{dataset_path}/data.yaml",
-    epochs = 50,
-    imgsz = 640,
-    batch = 16,
-    name = "single_components",
-    device = 1
-)
-model.export(format = "onnx")
+from multiprocessing import freeze_support
 
-metrics = model.val(data=f"{dataset_path}/data.yaml")
-print(metrics.box.map50)
-print(metrics.box.map)
-plot_results(file = "runs/detect/single_components/results.csv")
-plt.savefig("results.png")
-        
+if __name__=="__main__":
+    torch.cuda.empty_cache()  # clear memory
+
+    model = YOLO("yolov8n.pt")
+    model.to("cuda")
+    dataset_path = "../dataset"
+    results = model.train(
+        data = f"{dataset_path}/data.yaml",
+        epochs = 50,
+        imgsz = 120,
+        batch = 16,
+        name = "single_components",
+        device = 1
+    )
+    model.export(format = "onnx")
+
+    metrics = model.val(data=f"{dataset_path}/data.yaml")
+    print(metrics.box.map50)
+    print(metrics.box.map)
+    plot_results(file = "runs/detect/single_components/results.csv")
+    plt.savefig("results.png")
